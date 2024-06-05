7cec5393d92072a5914aac0dcef1abb5ce65abd6
keyushrashiya2002 <keyushrashiya2002@gmail.com>
2024-06-05 10:58:47 +0530

    create a API helper using axios

---
+++ src/helpers/api_helper.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { api } from "../config";
+
+// default
+axios.defaults.baseURL = api.API_URL;
+// content type
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+// content type
+const token = JSON.parse(sessionStorage.getItem("authUser"))
+  ? JSON.parse(sessionStorage.getItem("authUser")).token
+  : null;
+if (token) axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+
+const setAuthorization = (token) => {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+};
+
+class APIClient {
+  get = (url, params) => {
+    let response;
+
+    let paramKeys = [];
+
+    if (params) {
+      Object.keys(params).map((key) => {
+        paramKeys.push(key + "=" + params[key]);
+        return paramKeys;
+      });
+
+      const queryString =
+        paramKeys && paramKeys.length ? paramKeys.join("&") : "";
+      response = axios.get(`${url}?${queryString}`, params);
+    } else {
+      response = axios.get(`${url}`, params);
+    }
+
+    return response;
+  };
+  /**
+   * post given data to url
+   */
+  create = (url, data) => {
+    return axios.post(url, data);
+  };
+  /**
+   * Updates data
+   */
+  update = (url, data) => {
+    return axios.patch(url, data);
+  };
+
+  put = (url, data) => {
+    return axios.put(url, data);
+  };
+  /**
+   * Delete
+   */
+  delete = (url, config) => {
+    return axios.delete(url, { ...config });
+  };
+}
+
+export { APIClient, setAuthorization };
---
+++ src/helpers/backend_helper.jsx
@@ -0,0 +1,5 @@
+import { APIClient } from "./api_helper";
+
+import * as url from "./url_helper";
+
+const api = new APIClient();
